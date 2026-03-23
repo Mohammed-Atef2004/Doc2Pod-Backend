fe58b11734ea252f35f90d05ec71aa5d4bd09215
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2026-03-23 13:26:03 +0200

    editing assembly files

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481dae1cb97e501a26914fb74db25c589e890ee4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3534730d1a0a53a733245d71c4af80225299e1f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
